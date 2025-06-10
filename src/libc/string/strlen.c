de9b29a3e5e359fd1dbc3785d0afa10995ba69ba
RLGingerBiscuit <rlgingerbiscuit@proton.me>
2025-06-10 16:06:21 +0100

    Fix strlen

--- src/libc/string/strlen.c
+++ src/libc/string/strlen.c
@@ -3,7 +3,7 @@
 size_t strlen(const char *restrict str) {
   const char *s = str;
   const char *e = str;
-  for (; e != 0; e++) {
+  for (; *e != 0; e++) {
   }
-  return ((size_t)s - (size_t)e);
+  return ((size_t)e - (size_t)s);
 }
