df3412bce98076501937c7feffefeb01458d65b8
RLGingerBiscuit <rlgingerbiscuit@proton.me>
2025-06-14 23:07:15 +0100

    Only map .data/.bss sections as writable
    This took so long because I kept shooting myself in the foot.
    Funny how that works isn't it?
    
    When moving the MB adjustment code up, I didn't move the adjustment to
    the pointer itself, so I was then trying to write into some memory that
    clearly doesn't exist, which caused a triple fault..
    
    I also just completely forgot the moving code, so a test I had in
    `higher_half` was trying to write into some readonly memory
    (`0x0`, of course) which *also* caused a triple fault.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -7,8 +7,11 @@
 #include <kernel/tty.h>
 
 multiboot_info_t *multiboot_info_ptr;
+extern unsigned char _kernel_start[], _kernel_end[];
+extern unsigned char _kernel_readonly_start[], _kernel_readonly_end[];
+extern unsigned char _kernel_readwrite_start[], _kernel_readwrite_end[];
 
-void kernel_main(multiboot_info_t *mbd) {
+void kernel_main() {
   gdt_init();
 
   tty_init();
@@ -17,7 +20,8 @@ void kernel_main(multiboot_info_t *mbd) {
   printf("The number is %d\n", 42);
   printf("Multiboot: %p\n", multiboot_info_ptr);
   printf("Bootloader: %s\n", (char *)multiboot_info_ptr->boot_loader_name);
-  printf("Kernel Start:\t\t%p\tKernel End:\t\t%p\n", _kernel_start, _kernel_end);
+  printf("Kernel Start:\t\t%p\tKernel End:\t\t%p\n", _kernel_start,
+         _kernel_end);
   printf("Kernel RO Start:\t%p\tKernel RO End:\t%p\n", _kernel_readonly_start,
          _kernel_readonly_end);
   printf("Kernel RW Start:\t%p\tKernel RW End:\t%p\n", _kernel_readwrite_start,
